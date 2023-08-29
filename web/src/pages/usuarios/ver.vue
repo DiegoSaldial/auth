e3ebc04ef8f11cef87a444ba38b28695137a28eb
Tu Nombre <tu@email.com>
2023-08-29 18:23:15 -0400

    add view foto_url on usuarios view object

--- web/src/pages/usuarios/ver.vue
+++ web/src/pages/usuarios/ver.vue
@@ -7,8 +7,11 @@
       v-if="store.is_loading_page" />
 
     <div class="row justify-center q-mt-md">
-      <div class="col-12 col-md-6">
+      <div class="col-12 col-md-6 q-mb-md">
         <xobjeto :objeto="edit" />
+      </div>
+      <div v-if="edit?.foto_url" class="col-12 col-md-6 q-px-xl">
+        <q-img :src="edit?.foto_url" spinner-color="purple" xstyle="height: 50px; max-width: 50px" />
       </div>
     </div>
   </div>
